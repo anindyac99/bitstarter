a5f3feefe1b281c31631ae3ed1aa28761a3e025c
Anindya Chakraborty <anindyac99@gmail.com>
2013-07-14 23:12:13 +0000

    Modified Web.js to read from index.html

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World - Part 2!');
+	var fs= require('fs'); /Filesystem
+	var readBuffer = fs.readFileSync('index.html', 'utf-8');
+
+	response.send(readBuffer.toString());
 });
 
 var port = process.env.PORT || 5000;
