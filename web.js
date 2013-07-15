269bf61a33c166fefc47f699f6221d84f224022c
Anindya Chakraborty <anindyac99@gmail.com>
2013-07-15 00:12:46 +0000

    Fixed web.js

--- web.js
+++ web.js
@@ -3,8 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	var fs= require('fs'); /Filesystem
-	var readBuffer = fs.readFileSync('index.html', 'utf-8');
+	var fs = require('fs');
+	var readBuffer = fs.readFileSync('index.html', 'utf8');
 
 	response.send(readBuffer.toString());
 });
