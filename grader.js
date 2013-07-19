6b4e461217556309401485d160f9e508a31415b8
Anindya Chakraborty <anindyac99@gmail.com>
2013-07-19 07:30:00 +0000

    Final grader.js

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://evening-retreat-3431.herokuapp.com";
 
 var assertFileExists = function(infile) {
 	var instr = infile.toString();
@@ -35,6 +37,8 @@ var assertFileExists = function(infile) {
 	}
 	return instr;
 };
+
+var assertUrlExists = function (val) {return val.toString();};
 
 var cheerioHtmlFile = function(htmlfile) {
 	return cheerio.load(fs.readFileSync(htmlfile));
@@ -55,21 +59,46 @@ var checkHtmlFile = function(htmlfile, checksfile) {
 	return out;
 };
 
+
+var checkUrl = function(url, checksfile) {
+	$ = cheerio.load(url);
+	var checks = loadChecks(checksfile).sort();
+	var out = {};
+	for (var ii in checks) {
+		var present = $(checks[ii]).length > 0;
+		out[checks[ii]] = present;
+	};
+	return out;
+};
+
 var clone = function(fn) {
 	// Workaround for commander.js issue.
 	//     // http://stackoverflow.com/a/6772648
 	return fn.bind({});
 };
 
-
 if(require.main == module) {
 	program
 		.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 		.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+		.option('-u, --url <URL>', 'URL Name', clone(assertUrlExists), URL_DEFAULT)
 		.parse(process.argv);
-		var checkJson = checkHtmlFile(program.file, program.checks);
-		var outJson = JSON.stringify(checkJson, null, 4);
-		console.log(outJson);
-} else {
-		    exports.checkHtmlFile = checkHtmlFile;
-}
+		if (program.url) {
+		// If URL is provided - Get the entire HTML via restler
+			rest.get(program.url).on('complete', function(result) {
+				if (result instanceof Error) {
+					sys.puts('Error: ' + result.message);
+					this.retry(5000); // try again after 5 sec
+				} else {
+					var checkJson = checkUrl(result, program.checks);
+					var outJson = JSON.stringify(checkJson, null, 4);
+					console.log(outJson);
+				}
+			});
+		} else {
+			resultHtml = fs.readFileSync(program.file);
+			var checkJson = checkHtmlFile(program.file, program.checks);
+			var outJson = JSON.stringify(checkJson, null, 4);
+			console.log(outJson);
+		}
+} 
